dc54de998744561e7ee595059be444572fa4be85
Mostafa Gamal <moscd3@gmail.com>
2021-11-28 01:57:28 -0500

    Basic Tabs biolerplate

---
+++ app/src/main/java/com/scobrea/biometrx2/src/ui/fragments/LoginViewModel.kt
@@ -0,0 +1,7 @@
+package com.scobrea.biometrx2.src.ui.fragments
+
+import androidx.lifecycle.ViewModel
+
+class LoginViewModel : ViewModel() {
+    // TODO: Implement the ViewModel
+}
---
+++ app/src/main/java/com/scobrea/biometrx2/src/ui/fragments/SettingsViewModel.kt
@@ -0,0 +1,7 @@
+package com.scobrea.biometrx2.src.ui.fragments
+
+import androidx.lifecycle.ViewModel
+
+class SettingsViewModel : ViewModel() {
+    // TODO: Implement the ViewModel
+}
